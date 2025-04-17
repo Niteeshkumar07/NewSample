cd0a558bfcc91522ce5839fe33f195377212cbb6
Niteeshkumar07 <niteeshkumar4020@gmail.com>
2025-04-17 22:27:57 +0530

    New Changes Are Available

---
+++ src/ArithmeticStuff.java
@@ -0,0 +1,17 @@
+public class ArithmeticStuff {
+
+    public static void main(String []args){
+        int a=8;
+        int b=9;
+        System.out.println(a+b);
+        b=10;
+        System.out.println(a+b);
+
+        int x=5;
+        int y;
+        y= ++x + ++x;
+        System.out.println(y);
+
+
+    }
+}
