1664433181829840232753c83a790c51621d8ed6
Niteeshkumar07 <niteeshkumar4020@gmail.com>
2025-04-17 22:17:42 +0530

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+
+public class HelloWorld {
+
+    public static void main(String []args){
+//        System.out.println("hello world");
+//        String str="Niteesh Kumar";
+//        int a = 2;
+//        while(a<3){
+//            System.out.println(str);
+//            a++;
+//        Do some stuff in it
+
+        }
+
+
+    }
+
